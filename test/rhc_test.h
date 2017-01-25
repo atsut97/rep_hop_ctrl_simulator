5661e2ef4a59fc1e917e714fe6e87d3baac0f0f1
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-01-25 12:10:06 +0900

    Output failed messages after all tests are executed

--- test/rhc_test.h
+++ test/rhc_test.h
@@ -6,46 +6,56 @@
 
 #include <stdio.h>
 
-#define RHC_TEST_MESSAGE_MAX_LEN 1024
+#define RHC_TEST_FAILED_MESSAGE_NUM 64 /* TODO: Remove limit of message number */
+#define RHC_TEST_FAILED_MESSAGE_LEN 1024
 
 int rhc_test_run = 0;
 int rhc_test_fail = 0;
 int rhc_test_status = 0;
-char rhc_test_last_message[RHC_TEST_MESSAGE_MAX_LEN];
+char rhc_test_last_message[RHC_TEST_FAILED_MESSAGE_LEN];
+char rhc_test_messages[RHC_TEST_FAILED_MESSAGE_NUM][RHC_TEST_FAILED_MESSAGE_LEN];
 
 #define RHC_TEST(test_name) static void test_name()
 #define RHC_TEST_SUITE(suite_name) static void suite_name()
 
 #define RHC_ASSERT(test, message) do{\
   if( !(test) ){\
-    rhc_test_fail++;\
-    printf( "F" );\
     rhc_test_status = 1;\
-    sprintf( rhc_test_last_message, "%s", message );\
-  } else\
-    printf( "." );\
+    if( rhc_test_fail < RHC_TEST_FAILED_MESSAGE_NUM - 1 )\
+      snprintf( rhc_test_messages[rhc_test_fail], RHC_TEST_FAILED_MESSAGE_LEN, "FAIL: %s (%s:%d)\n%s\n", __FUNCTION__, __FILE__, __LINE__, message );\
+    else\
+      snprintf( rhc_test_messages[RHC_TEST_FAILED_MESSAGE_NUM-1], RHC_TEST_FAILED_MESSAGE_LEN, "Too many failures! (subsequent messages are skipped)\n" );\
+    rhc_test_fail++;\
+  }\
 } while( 0 )
 
 #define RHC_RUN_TEST(test_name) do{\
   rhc_test_run++;\
   rhc_test_status = 0;\
   test_name();\
-  if( rhc_test_status ){\
-    printf( "\nFAIL: %s (%s:%d)\n%s\n", #test_name, __FILE__, __LINE__, rhc_test_last_message );\
-  }\
+  if( rhc_test_status ) printf( "F" );\
+  else printf( "." );\
 } while( 0 )
 
 #define RHC_RUN_SUITE(suite_name) do{\
   suite_name();\
 } while( 0 )
 
+#define RHC_TEST_SEPARATOR1 "======================================================================\n"
+#define RHC_TEST_SEPARATOR2 "----------------------------------------------------------------------\n"
+
 #define RHC_TEST_REPORT() do{\
-  printf( "\n----------------------------------------------------------------------");\
-  printf( "\nRan %d tests\n", rhc_test_run );\
+  int __rhc_test_iter;\
+  for( __rhc_test_iter=0; __rhc_test_iter<rhc_test_fail && __rhc_test_iter<RHC_TEST_FAILED_MESSAGE_NUM; __rhc_test_iter++){\
+    printf( "\n" RHC_TEST_SEPARATOR1 );\
+    printf( "%s", rhc_test_messages[__rhc_test_iter] );\
+  }\
+  printf( "\n" RHC_TEST_SEPARATOR2 );\
+  printf( "Ran %d tests\n\n", rhc_test_run );\
   if( rhc_test_fail == 0 ){\
-    printf( "\nOK\n" );\
+    printf( "OK\n" );\
   } else {\
-    printf( "\nFAILED (failures=%d)\n", rhc_test_fail );\
+    printf( "FAILED (failures=%d)\n", rhc_test_fail );\
   }\
 } while( 0 )
 
