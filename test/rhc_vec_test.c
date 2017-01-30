68f6e62e049ea65addc125ebe4201892ab3fe631
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-01-30 14:59:14 +0900

    Add tests for method 'rhcVecSetElem'

--- test/rhc_vec_test.c
+++ test/rhc_vec_test.c
@@ -1,8 +1,8 @@
-#include "rhc_test.h"
 #include "rhc_vec.h"
+#include "rhc_test.h"
 #include <malloc.h>
 
-void _test_vec_create(uint size)
+void check_if_vec_create(uint size)
 {
   rhcVec v;
 
@@ -14,13 +14,45 @@ void _test_vec_create(uint size)
 
 RHC_TEST(test_vec_create)
 {
-  _test_vec_create( 2 );
-  _test_vec_create( 5 );
+  check_if_vec_create( 2 );
+  check_if_vec_create( 5 );
+}
+
+void check_if_vec_set_elem(rhcVec v, uint size, ... )
+{
+  va_list args;
+  double val;
+  register uint i;
+
+  va_start( args, size );
+  for( i=0; i<size; i++ ){
+    val = (double)va_arg( args, double );
+    RHC_ASSERT_EQ( val, rhcVecElem( v, i ) );
+  }
+  va_end( args );
+}
+
+RHC_TEST(test_vec_set_elem)
+{
+  rhcVec v;
+
+  v = rhcVecCreate( 1 );
+  rhcVecSetElem( v, 0, 1.0 );
+  check_if_vec_set_elem( v, 1, 1.0 );
+  rhcVecDestroy( v );
+
+  v = rhcVecCreate( 3 );
+  rhcVecSetElem( v, 0, 2 );
+  rhcVecSetElem( v, 1, 3.0 );
+  rhcVecSetElem( v, 2, 5.5 );
+  check_if_vec_set_elem( v, 3, 2.0, 3.0, 5.5 );
+  rhcVecDestroy( v );
 }
 
 RHC_TEST_SUITE(test_vec)
 {
   RHC_RUN_TEST(test_vec_create);
+  RHC_RUN_TEST(test_vec_set_elem);
 }
 
 int main(int argc, char *argv[])
