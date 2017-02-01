2abbf86e46943909cd846ddec7809a20df533eab
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-01 10:41:51 +0900

    misc: Fix error message utilities

--- include/rhc_misc.h
+++ include/rhc_misc.h
@@ -10,21 +10,24 @@
 /* for error messages */
 #define eprintf(fmt,...) fprintf( stderr, fmt, ##__VA_ARGS__ )
 
-static bool __err_echo = true;
-static char __err_last_msg[BUFSIZ];
+extern bool __err_echo;
+char __err_last_msg[BUFSIZ];
 #define ECHO_ON()  ( __err_echo = true )
 #define ECHO_OFF() ( __err_echo = false )
 
 #define RUNTIME_ERR(msg,...) do{\
-  snprintf( __err_last_msg, BUFSIZ, "Error: " msg "(%s)\n", ##__VA_ARGS__, __FUNCTION__ );\
-  __err_echo ? eprintf( "%s", __err_last_msg ) : 0;\
+  snprintf( __err_last_msg, BUFSIZ, "%s", msg );\
+  __err_echo ? eprintf( "Error: %s(%s)\n", ##__VA_ARGS__, __err_last_msg, __FUNCTION__ ) : 0;\
 } while( 0 )
 #define RUNTIME_WARN(msg,...) do{\
-  snprintf( __err_last_msg, BUFSIZ, "Warning: " msg "(%s)\n", ##__VA_ARGS__, __FUNCTION__ );\
-  __err_echo ? eprintf( "%s", __err_last_msg ) : 0;\
+  snprintf( __err_last_msg, BUFSIZ, "%s", msg );\
+  __err_echo ? eprintf( "Warning: %s(%s)\n", ##__VA_ARGS__, __err_last_msg, __FUNCTION__ ) : 0;\
 } while( 0 )
+#define RESET_ERR_MSG() ( __err_last_msg[0] = '\0' )
 
-#define ALLOC_ERR() RUNTIME_ERR( "cannot allocate memory" )
-#define OPEN_ERR(m) RUNTIME_ERR( "cannot open file: %s", (m) )
+#define ALLOC_ERR_MSG "cannot allocate memory"
+#define OPEN_ERR_MSG  "cannot open file"
+#define ALLOC_ERR() RUNTIME_ERR( ALLOC_ERR_MSG )
+#define OPEN_ERR(m) RUNTIME_ERR( OPEN_ERR_MSG ": %s", (m) )
 
 #endif /* __RHC_MISC_H__ */
---
+++ src/rhc_misc.c
@@ -0,0 +1,3 @@
+#include "rhc_misc.h"
+
+bool __err_echo = true;
