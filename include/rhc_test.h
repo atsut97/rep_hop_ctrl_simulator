e2132097731bc5da64ad82d2831b51fa5137b664
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-01 13:04:47 +0900

    test: Fix failure message
    Previous version made failure message show function name from which
    is called, but it is confused when a helper function asserts error.

--- include/rhc_test.h
+++ include/rhc_test.h
@@ -14,9 +14,9 @@
 #include <math.h>
 #include <string.h>
 
-#define TEST_FAILED_MESSAGE_NUM 64
-#define TEST_FAILED_MESSAGE_LEN 1024
-#define TEST_TOL ( 1.0e-12 )
+#define TEST_BUFSIZ      1024
+#define TEST_N_MSG_STACK 64
+#define TEST_TOL         ( 1.0e-12 )
 
 #define TEST_SEPARATOR1 "======================================================================\n"
 #define TEST_SEPARATOR2 "----------------------------------------------------------------------\n"
@@ -30,8 +30,9 @@ static int __test_status = 0;
 static double __test_timer_time = 0;
 
 /* Message */
-static char __test_last_message[TEST_FAILED_MESSAGE_LEN];
-static char __test_messages[TEST_FAILED_MESSAGE_NUM][TEST_FAILED_MESSAGE_LEN];
+static char __test_last_message[TEST_BUFSIZ];
+static char __test_messages[TEST_N_MSG_STACK][TEST_BUFSIZ];
+static char __test_running_test_name[TEST_BUFSIZ];
 
 /* Setup and teardown function */
 static void (*__test_setup)() = NULL;
@@ -41,10 +42,10 @@ static void (*__test_teardown)() = NULL;
 #define TEST_SUITE(suite_name) static void suite_name()
 
 #define STACK_FAILURE_MESSAGE(message) do{\
-  if( __test_fail < TEST_FAILED_MESSAGE_NUM - 1 )\
-    snprintf( __test_messages[__test_fail], TEST_FAILED_MESSAGE_LEN, "FAIL: %s (%s:%d)\n%s%s\n", __FUNCTION__, __FILE__, __LINE__, TEST_SEPARATOR2, message );\
+  if( __test_fail < TEST_N_MSG_STACK - 1 )\
+    snprintf( __test_messages[__test_fail], TEST_BUFSIZ, "FAIL: %s (%s:%d)\n%s%s\n", __test_running_test_name, __FILE__, __LINE__, TEST_SEPARATOR2, message );\
   else\
-    snprintf( __test_messages[TEST_FAILED_MESSAGE_NUM-1], TEST_FAILED_MESSAGE_LEN, "Too many failures! (subsequent messages are skipped)\n" );\
+    snprintf( __test_messages[TEST_N_MSG_STACK-1], TEST_BUFSIZ, "Too many failures! (subsequent messages are skipped)\n" );\
 } while( 0 )
 
 #define ASSERT(test, message) do{\
@@ -57,19 +58,19 @@ static void (*__test_teardown)() = NULL;
 } while( 0 )
 
 #define ASSERT_TRUE(cond) do{\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of %s\nExpected: true\n But was: false", #cond );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Value of %s\nExpected: true\n But was: false", #cond );\
   ASSERT( (cond), __test_last_message );\
 } while( 0 )
 
 #define ASSERT_FALSE(cond) do{\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of %s\nExpected: false\n But was: true", #cond );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Value of %s\nExpected: false\n But was: true", #cond );\
   ASSERT( !(cond), __test_last_message );\
 } while( 0 )
 
 #define ASSERT_EQ(expected, actual) do{\
   if( (expected) != (actual) ){\
     __test_status = 1;\
-    snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of: %s\nExpected: %g\n But was: %g", #actual, (double)expected, (double)actual );\
+    snprintf( __test_last_message, TEST_BUFSIZ, "Value of: %s\nExpected: %g\n But was: %g", #actual, (double)expected, (double)actual );\
     STACK_FAILURE_MESSAGE( __test_last_message );\
     __test_fail++;\
     return;\
@@ -81,7 +82,7 @@ static void (*__test_teardown)() = NULL;
   double __actual_double   = (actual);\
   if( fabs( (__expected_double) - (__actual_double) ) > TEST_TOL ){\
     __test_status = 1;\
-    snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of: %s\nExpected: %g\n But was: %g", #actual, __expected_double, __actual_double);\
+    snprintf( __test_last_message, TEST_BUFSIZ, "Value of: %s\nExpected: %g\n But was: %g", #actual, __expected_double, __actual_double);\
     STACK_FAILURE_MESSAGE( __test_last_message );\
     __test_fail++;\
     return;\
@@ -93,7 +94,7 @@ static void (*__test_teardown)() = NULL;
   double __tmp_val2 = (val2);\
   if( !( __tmp_val1 > __tmp_val2 ) ){\
   __test_status = 1;\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Expected: (%s) > (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Expected: (%s) > (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
   STACK_FAILURE_MESSAGE( __test_last_message );\
   __test_fail++;\
   return;\
@@ -105,7 +106,7 @@ static void (*__test_teardown)() = NULL;
   double __tmp_val2 = (val2);\
   if( !( __tmp_val1 >= __tmp_val2 ) ){\
   __test_status = 1;\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Expected: (%s) >= (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Expected: (%s) >= (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
   STACK_FAILURE_MESSAGE( __test_last_message );\
   __test_fail++;\
   return;\
@@ -117,7 +118,7 @@ static void (*__test_teardown)() = NULL;
   double __tmp_val2 = (val2);\
   if( !( __tmp_val1 < __tmp_val2 ) ){\
   __test_status = 1;\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Expected: (%s) < (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Expected: (%s) < (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
   STACK_FAILURE_MESSAGE( __test_last_message );\
   __test_fail++;\
   return;\
@@ -129,7 +130,7 @@ static void (*__test_teardown)() = NULL;
   double __tmp_val2 = (val2);\
   if( !( __tmp_val1 <= __tmp_val2 ) ){\
   __test_status = 1;\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Expected: (%s) <= (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Expected: (%s) <= (%s)\n But was: %g vs %g", #val1, #val2, __tmp_val1, __tmp_val2 );\
   STACK_FAILURE_MESSAGE( __test_last_message );\
   __test_fail++;\
   return;\
@@ -139,7 +140,7 @@ static void (*__test_teardown)() = NULL;
 #define ASSERT_STREQ(expected, actual) do{\
   if( strcmp( (expected), (actual) ) ){\
   __test_status = 1;\
-  snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of: %s\nExpected: %s\n But was: %s", #actual, expected, actual );\
+  snprintf( __test_last_message, TEST_BUFSIZ, "Value of: %s\nExpected: %s\n But was: %s", #actual, expected, actual );\
   STACK_FAILURE_MESSAGE( __test_last_message );\
   __test_fail++;\
   return;\
@@ -149,7 +150,7 @@ static void (*__test_teardown)() = NULL;
 #define ASSERT_PTREQ(expected, actual) do{\
   if( (expected) != (actual) ){\
     __test_status = 1;\
-    snprintf( __test_last_message, TEST_FAILED_MESSAGE_LEN, "Value of: %s\nExpected: %p\n But was: %p", #actual, expected, actual );\
+    snprintf( __test_last_message, TEST_BUFSIZ, "Value of: %s\nExpected: %p\n But was: %p", #actual, expected, actual );\
     STACK_FAILURE_MESSAGE( __test_last_message );\
     __test_fail++;\
     return;\
@@ -158,6 +159,7 @@ static void (*__test_teardown)() = NULL;
 
 #define RUN_TEST(test_name) do{\
   if( __test_timer_time == 0 ) __test_timer_time = test_timer();\
+  snprintf( __test_running_test_name, TEST_BUFSIZ, "%s", #test_name );\
   if( __test_setup ) (*__test_setup)();\
   __test_status = 0;\
   test_name();\
@@ -182,7 +184,7 @@ static void (*__test_teardown)() = NULL;
   double __test_timer_end;\
   int __test_iter;\
   __test_timer_end = test_timer();\
-  for( __test_iter=0; __test_iter<__test_fail && __test_iter<TEST_FAILED_MESSAGE_NUM; __test_iter++){\
+  for( __test_iter=0; __test_iter<__test_fail && __test_iter<TEST_N_MSG_STACK; __test_iter++){\
     printf( "\n" TEST_SEPARATOR1 );\
     printf( "%s", __test_messages[__test_iter] );\
   }\
