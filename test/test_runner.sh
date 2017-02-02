dd6560959f44cf163cd9537cb7b1cbab83b590aa
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-02 21:13:38 +0900

    test: Improve test_runner
    Make it possible to check memory leak using valgrind

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+# usage
+usage () {
+    echo "./test_runner <options> test1 test2 ..."
+    echo "Arguments:"
+    echo "\tSpecify tests to be executed. If none of tests"
+    echo "\tis specified all the tests will be executed."
+    echo ""
+    echo "Options:"
+    echo "\t-h|-help      show this message and exit."
+    echo "\t-m|-memcheck  check memory leak using valgrind."
+}
+
+# parse arguments
+MEMCHECK=false
+TESTS=
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -help | -h)     usage
+                        exit 0
+                        ;;
+        -memcheck | -m) MEMCHECK=true
+                        shift
+                        ;;
+        -*)             usage
+                        exit 1
+                        ;;
+        *)              TESTS="$TESTS ./$1"
+                        shift
+                        ;;
+    esac
+done
+
 # find all executable files (excluding script files etc.)
 # arguments: none
 # output: found exes are output to stdout
@@ -24,32 +56,28 @@ contain () {
     return 1
 }
 
+# check if a command is available
+command_exist () {
+    command -v $1 >/dev/null 2>&1
+    ret=$?
+    if [ $ret -gt 0 ]; then
+        echo "$1 is not available"
+        echo "try 'sudo apt-get install $1'"
+    fi
+    return $ret
+}
+
 # echo running test name and run the test
 # argumets: a test to run
 # output: return value of the test
 run_test () {
     test_name=`echo $i | sed -e "s/^..//g"`
     echo "\nRunning $test_name..."
-    $1
+    $VALGRIND$1
     return $?
 }
 
-TESTS=`find_exe .`
-SKIPPED_TESTS="./rhc_test_test"
-
-FAILED_TESTS=""
-for i in $TESTS; do
-    ret=$(contain $i $SKIPPED_TESTS; echo $?)
-    if [ $ret -eq 0 ]; then
-        # echo "$i is skipped"
-        continue
-    fi
-    run_test $i
-    if [ $? -gt 0 ]; then
-        FAILED_TESTS="$FAILED_TESTS $test_name"
-    fi
-done
-
+# report all of the arguments as failed
 report_failed_tests () {
     printf "\n"
     printf "\033[31m==============================\n"
@@ -60,6 +88,7 @@ report_failed_tests () {
     printf "\033[31m==============================\n"
 }
 
+# show tests are passed
 report_all_green () {
     printf "\n"
     printf "\033[32mAll green!\n"
@@ -74,5 +103,38 @@ report () {
         report_all_green
     fi
 }
+
+#
+# main part starts from here
+#
+
+# if no test is specified find all tests
+if [ -z $TESTS ]; then
+    TESTS=`find_exe .`
+fi
+# check memcheck flag
+VALGRIND=
+if [ $MEMCHECK = true ]; then
+    command_exist valgrind
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        VALGRIND="valgrind --leak-check=full "
+    fi
+fi
+
+SKIPPED_TESTS="./rhc_test_test"
+FAILED_TESTS=""
+for i in $TESTS; do
+    contain $i $SKIPPED_TESTS
+    ret=$?
+    if [ $ret -eq 0 ]; then
+        # echo "$i is skipped"
+        continue
+    fi
+    run_test $i
+    if [ $? -gt 0 ]; then
+        FAILED_TESTS="$FAILED_TESTS $test_name"
+    fi
+done
 
 report $FAILED_TESTS
