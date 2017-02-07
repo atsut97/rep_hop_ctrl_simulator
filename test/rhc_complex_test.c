74dada2c09a6ee9f6a0f0671ec5154652697ad1f
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-07 16:52:18 +0900

    complex: Refactor tests

--- test/rhc_complex_test.c
+++ test/rhc_complex_test.c
@@ -1,13 +1,42 @@
 #include "rhc_complex.h"
 #include "rhc_test.h"
 
-void check_complex_init(double re, double im)
+void check_eq_complex(complex_t *expected, complex_t *actual)
 {
-  complex_t c;
+  ASSERT_DOUBLE_EQ( expected->re, actual->re );
+  ASSERT_DOUBLE_EQ( expected->im, actual->im );
+}
 
-  complex_init( &c, re, im );
-  ASSERT_EQ( re, c.re );
-  ASSERT_EQ( im, c.im );
+void check_method_dd_c(double arg1, double arg2, complex_t *expected, complex_t* (*method)(complex_t*,double,double))
+{
+  complex_t actual;
+
+  method( &actual, arg1, arg2 );
+  check_eq_complex( expected, &actual );
+}
+
+void check_method_c_c(complex_t *arg, complex_t *expected, complex_t* (*method)(complex_t*,complex_t*))
+{
+  complex_t actual;
+
+  method( arg, &actual );
+  check_eq_complex( expected, &actual );
+}
+
+void check_method_c_d(complex_t *arg, double expected, double (*method)(complex_t*))
+{
+  double actual;
+
+  actual = method( arg );
+  ASSERT_DOUBLE_EQ( expected, actual );
+}
+
+void check_method_cc_c(complex_t *arg1, complex_t *arg2, complex_t *expected, complex_t* (*method)(complex_t*,complex_t*,complex_t*))
+{
+  complex_t actual;
+
+  method( arg1, arg2, &actual );
+  check_eq_complex( expected, &actual );
 }
 
 TEST(test_complex_init)
@@ -15,27 +44,19 @@ TEST(test_complex_init)
   struct case_t {
     double re;
     double im;
+    complex_t expected;
     bool end;
   } cases[] = {
-    { 0, 0, false },
-    { 1, 3, false },
-    { -3, 2, false },
-    { 1, -2, false },
-    { -8, -6, false },
-    { 0, 0, true } };
+    { 0, 0, { 0, 0 }, false },
+    { 1, 3, { 1, 3 }, false },
+    { -3, 2, { -3, 2 }, false },
+    { 1, -2, { 1, -2 }, false },
+    { -8, -6, { -8, -6 }, false },
+    { 0, 0, {}, true } };
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_init( c->re, c->im );
-}
-
-void check_complex_polar(double r, double theta, double re, double im)
-{
-  complex_t c;
-
-  complex_polar( &c, r, theta );
-  ASSERT_DOUBLE_EQ( re, c.re );
-  ASSERT_DOUBLE_EQ( im, c.im );
+    check_method_dd_c( c->re, c->im, &c->expected, complex_init );
 }
 
 TEST(test_complex_polar)
@@ -43,19 +64,18 @@ TEST(test_complex_polar)
   struct case_t {
     double r;
     double theta;
-    double re;
-    double im;
+    complex_t expected;
     bool end;
   } cases[] = {
-    { 0, 0,    0, 0, false },
-    { 1, 0,    1, 0, false },
-    { 2, PI_2, 0, 2, false },
-    { 1, PI,  -1, 0, false },
-    { 0, 0,    0, 0, true } };
+    { 0, 0,    { 0, 0 }, false },
+    { 1, 0,    { 1, 0 }, false },
+    { 2, PI_2, { 0, 2 }, false },
+    { 1, PI,   {-1, 0 }, false },
+    { 0, 0,    { 0, 0 }, true } };
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_polar( c->r, c->theta, c->re, c->im );
+    check_method_dd_c( c->r, c->theta, &c->expected, complex_polar);
 }
 
 TEST(test_complex_clear)
@@ -79,20 +99,20 @@ void check_complex_sqr_abs(double re, double im, double expected)
 TEST(test_complex_sqr_abs)
 {
   struct case_t {
-    double re, im;
+    complex_t arg;
     double expected;
     bool end;
   } cases[] = {
-    {  0, 0,    0, false },
-    {  1, 1,    2, false },
-    { -3, 4,   25, false },
-    { -5, 12, 169, false },
-    { 0, 0, 0, true },
+    { { 0, 0 },    0, false },
+    { { 1, 1 },    2, false },
+    { {-3, 4 },   25, false },
+    { {-5, 12 }, 169, false },
+    { { 0, 0 },    0, true },
   };
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_sqr_abs( c->re, c->im, c->expected );
+    check_method_c_d( &c->arg, c->expected, complex_sqr_abs );
 }
 
 void check_complex_abs(double re, double im, double expected)
@@ -106,63 +126,46 @@ void check_complex_abs(double re, double im, double expected)
 TEST(test_complex_abs)
 {
   struct case_t {
-    double re, im;
+    complex_t arg;
     double expected;
     bool end;
   } cases[] = {
-    {  0, 0,       0, false },
-    {  1, 1, sqrt(2), false },
-    { -3, 4,       5, false },
-    { -5, 12,     13, false },
-    { 0, 0, 0, true },
+    { { 0, 0 },       0, false },
+    { { 1, 1 }, sqrt(2), false },
+    { {-3, 4 },       5, false },
+    { {-5, 12 },     13, false },
+    { { 0, 0 },       0, true },
   };
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_abs( c->re, c->im, c->expected );
-}
-
-void check_complex_arg(double re, double im, double expected)
-{
-  complex_t c;
-
-  complex_init( &c, re, im );
-  ASSERT_DOUBLE_EQ( expected, complex_arg( &c ) );
+    check_complex_abs( c->arg.re, c->arg.im, c->expected );
 }
 
 TEST(test_complex_arg)
 {
   struct case_t {
-    double re, im;
+    complex_t arg;
     double expected;
     bool end;
   } cases[] = {
-    {  0, 0, 0, false },
-    {  1, 1, PI/4, false },
-    {  1, sqrt(3), PI/3, false },
-    { -1, 1, 3*PI/4, false },
-    { -sqrt(3), -1, -5*PI/6, false },
-    { 0, 0, 0, true },
+    { { 0, 0 }, 0, false },
+    { { 1, 1 }, PI/4, false },
+    { { 1, sqrt(3) }, PI/3, false },
+    { {-1, 1 }, 3*PI/4, false },
+    { {-sqrt(3), -1 }, -5*PI/6, false },
+    { {0, 0 }, 0, true },
   };
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_arg( c->re, c->im, c->expected );
-}
-
-void check_complex_conj(complex_t *c1, complex_t *expected)
-{
-  complex_t c2;
-
-  complex_conj( c1, &c2 );
-  ASSERT_DOUBLE_EQ( expected->re, c2.re );
-  ASSERT_DOUBLE_EQ( expected->im, c2.im );
+    check_method_c_d( &c->arg, c->expected, complex_arg );
 }
 
 TEST(test_complex_conj)
 {
   struct case_t {
-    complex_t c1;
+    complex_t arg;
     complex_t expected;
     bool end;
   } cases[] = {
@@ -176,7 +179,7 @@ TEST(test_complex_conj)
   struct case_t *c;
 
   for( c=cases; !c->end; c++ )
-    check_complex_conj( &c->c1, &c->expected );
+    check_method_c_c( &c->arg, &c->expected, complex_conj );
 }
 
 TEST_SUITE(test_complex)
