4fec60ac56b88822fa89fc8926f4d4e54b4ba79c
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-08 18:58:07 +0900

    model: Implement 'model_destroy'

--- src/rhc_model.c
+++ src/rhc_model.c
@@ -9,4 +9,6 @@ model_t *model_init(model_t *self, double m, cmd_t *cmd)
 
 void model_destroy(model_t *self)
 {
+  self->m = 0;
+  self->cmd = NULL;
 }
--- test/rhc_model_test.c
+++ test/rhc_model_test.c
@@ -11,9 +11,21 @@ TEST(test_model_init)
   ASSERT_PTREQ( &cmd, model_cmd( &m ) );
 }
 
+TEST(test_model_destroy)
+{
+  model_t m;
+  cmd_t cmd;
+
+  model_init( &m, 1.0, &cmd );
+  model_destroy( &m );
+  ASSERT_EQ( 0.0, model_mass( &m ) );
+  ASSERT_PTREQ( NULL, model_cmd( &m ) );
+}
+
 TEST_SUITE(test_model)
 {
   RUN_TEST(test_model_init);
+  RUN_TEST(test_model_destroy);
 }
 
 int main(int argc, char *argv[])
