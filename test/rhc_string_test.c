79c3b35966a1e6de82d074c70a465252ba1a0252
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-07-12 22:10:21 +0900

    string: Add string class

---
+++ test/rhc_string_test.c
@@ -0,0 +1,34 @@
+#include "rhc_string.h"
+#include "rhc_test.h"
+
+void setup()
+{
+}
+
+void teardown()
+{
+}
+
+TEST(test_string_copy)
+{
+  char s[] = "test string";
+  char d[BUFSIZ];
+  char *ret;
+
+  ret = string_copy(s, d);
+  ASSERT_STREQ("test string", d);
+  ASSERT_PTREQ(d, ret);
+}
+
+TEST_SUITE(test_string)
+{
+  CONFIGURE_SUITE( setup, teardown );
+  RUN_TEST(test_string_copy);
+}
+
+int main(int arg, char *argv[])
+{
+  RUN_SUITE(test_string);
+  TEST_REPORT();
+  TEST_EXIT();
+}
