131dfc196d8d437b66ec2a886daeec649077737f
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-07-12 22:55:23 +0900

    logger: Fix coding style

--- test/rhc_logger_test.c
+++ test/rhc_logger_test.c
@@ -14,29 +14,29 @@ void teardown()
 TEST(test_logger_init)
 {
   logger_t l;
-  logger_init(&l, "/tmp/test.log");
+  logger_init( &l, "/tmp/test.log" );
   ASSERT_STREQ( "/tmp/test.log", logger_filename(&l) );
-  ASSERT_PTRNE(NULL, l.fp);
+  ASSERT_PTRNE( NULL, l.fp );
 }
 
 TEST(test_logger_destroy)
 {
   logger_t l;
-  logger_init(&l, "/tmp/test.log");
-  logger_destroy(&l);
-  ASSERT_PTREQ(NULL, l.fp);
+  logger_init( &l, "/tmp/test.log" );
+  logger_destroy( &l );
+  ASSERT_PTREQ( NULL, l.fp );
 }
 
 TEST_SUITE(test_logger)
 {
   CONFIGURE_SUITE( setup, teardown );
-  RUN_TEST(test_logger_init);
-  RUN_TEST(test_logger_destroy);
+  RUN_TEST( test_logger_init );
+  RUN_TEST( test_logger_destroy );
 }
 
 int main(int argc, char *argv[])
 {
-  RUN_SUITE(test_logger);
+  RUN_SUITE( test_logger );
   TEST_REPORT();
   TEST_EXIT();
 }
