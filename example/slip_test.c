7f3a6055609be13bab270a382395249a3b55aede
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-07-26 16:31:06 +0900

    ctrl_slip: Fix test to output lots of hopping results

--- example/slip_test.c
+++ example/slip_test.c
@@ -26,7 +26,8 @@ int main(int argc, char *argv[])
   ctrl_t ctrl;
   logger_t logger;
   simulator_t sim;
-  vec_t p;
+  vec_t p0;
+  double z0;
 
   cmd_default_init( &cmd );
   model_init( &model, 10 );
@@ -34,11 +35,14 @@ int main(int argc, char *argv[])
   logger_init( &logger );
   simulator_init( &sim, &cmd, &ctrl, &model );
   simulator_set_default_logger( &sim, &logger );
-  p = vec_create_list( 2, 0.28, 0.0 );
+  p0 = vec_create( 2 );
 
-  simulator_run( &sim, p, T, DT, &logger, NULL );
+  for( z0 = cmd.z0 - 0.005; z0 < cmd.zd + 0.1; z0 += 0.005 ) {
+    vec_set_elem_list( p0, z0, 0.0 );
+    simulator_run( &sim, p0, T, DT, &logger, NULL );
+  }
 
-  vec_destroy( p );
+  vec_destroy( p0 );
   simulator_destroy( &sim );
   logger_destroy( &logger );
   ctrl_destroy( &ctrl );
