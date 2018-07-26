c3de6785be6140c942f622527cf67989c6d26ecb
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-07-26 16:30:50 +0900

    example: Fix script to run test

--- example/run_test.sh
+++ example/run_test.sh
@@ -3,12 +3,12 @@
 if [ -z $1 ]; then
   echo "please specify test program to run"
   echo "for example,"
-  echo "  ./run_test.sh ./slip_test"
+  echo "  ./run_test.sh slip_test"
   exit 1
 fi
 
 make
-$1 > data.csv
+./$1 > $1.csv
 if [ -f Pipfile ]; then
-  pipenv run ./plot_one_hop.py data.csv
+  pipenv run ./plot_one_hop.py $1.csv
 fi
