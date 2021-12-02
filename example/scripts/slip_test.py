c8b13492686bfbc3a482f8f6b42dca651b7f7c29
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-12-02 18:58:00 +0900

    ctrl_slip: Set limits to axes for plotting

--- example/scripts/slip_test.py
+++ example/scripts/slip_test.py
@@ -8,9 +8,11 @@ from lib.datautil import DataGroup
 
 def plot(datagroup):
     data = datagroup.get("sc00005")
-    z0 = data.param("z0")
+    z0, zd, zb = data.param(["z0", "zd", "zb"])
     plt.axvline(z0, lw=1, c="k")
     plt.axhline(0, lw=1, c="k")
+    plt.xlim([zb - 0.01, zd + 0.01])
+    plt.ylim([-0.8, 0.8])
     plt.plot(data.z, data.vz)
 
 
