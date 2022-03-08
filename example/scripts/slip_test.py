abd468fa06122df4262f8a7e026347bd6b1c7c31
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2022-03-08 11:07:45 +0900

    example: Ignore type check error

--- example/scripts/slip_test.py
+++ example/scripts/slip_test.py
@@ -8,7 +8,7 @@ from pyplotutil.datautil import DataSet
 def plot(dataset: DataSet) -> None:
     data = dataset.get("sc00005")
     z0, zd, zb = data.param(["z0", "zd", "zb"])
-    plt.axvline(z0, lw=1, c="k")
+    plt.axvline(z0, lw=1, c="k")  # type: ignore
     plt.axhline(0, lw=1, c="k")
     plt.xlim([zb - 0.01, zd + 0.01])
     plt.ylim([-0.8, 0.8])
