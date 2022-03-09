c48f546790b7d33c5077e5cd01a0cabaab7cfc25
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2022-03-09 18:39:00 +0900

    example: Add script to plot a few trajectories

---
+++ example/scripts/simple_plot.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+from pyplotutil.datautil import DataSet
+
+
+def load(argv: list[str]) -> tuple[DataSet, list[str]]:
+    dataset = DataSet(argv[1])
+    tags = []
+    if len(argv) == 2:
+        # No tags specified.
+        tags.append(dataset.tags()[0])
+    else:
+        for tag in argv[2:]:
+            if tag.isdigit():
+                tags.append(dataset.tags()[int(tag)])
+            else:
+                tags.append(tag)
+    return dataset, tags
+
+
+def plot(dataset: DataSet, tags: list[str]) -> None:
+    # Prepare figure.
+    _, ax = plt.subplots()
+    ax.set_xlabel("position [m]")
+    ax.set_ylabel("velocity [m/s]")
+    if dataset.datapath:
+        ax.set_title(dataset.datapath.name)
+
+    # Plot constant parameters.
+    z0, zd, zb = dataset.param(["z0", "zd", "zb"])
+    ax.axvline(z0, ls="-", lw=1, c="k")
+    ax.axhline(0, ls="-", lw=1, c="k")
+    ax.axvline(zd, ls="--", lw=1, c="k")
+    ax.axvline(zb, ls="--", lw=1, c="k")
+
+    # Plot a specified trajectory.
+    for tag in tags:
+        data = dataset.get(tag)
+        ax.plot(data.z, data.vz, lw=1, label=tag)
+
+    # Set miscellaneous parameters.
+    ax.grid()
+    ax.legend()
+
+
+def usage(prog) -> None:
+    msg = f"""\
+Usage:
+    {prog} <data.csv> [index...]
+
+Arguments:
+    data.csv (required):
+        CSV format data file.
+    index (optional):
+        Data index (integer) or tag string.
+        Multiple indices or tags can be specified.
+"""
+    print(msg)
+
+
+def parse(argv: list[str]) -> bool:
+    if len(argv) == 1 or argv[1] in ["-h", "-help", "--help"]:
+        usage(argv[0])
+        return False
+
+    return True
+
+
+def main() -> None:
+    import sys
+
+    if not parse(sys.argv):
+        exit(1)
+
+    dataset, tag = load(sys.argv)
+    plot(dataset, tag)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
