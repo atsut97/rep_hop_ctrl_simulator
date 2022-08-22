accb14700c7a983788a239aaababf034eb75c586
Hiroshi Atsuta <atsuta@ieee.org>
2022-08-22 15:50:11 +0900

    mtoka_osci: Add getter/setter of parameters

--- include/rhc_matsuoka_oscillator.h
+++ include/rhc_matsuoka_oscillator.h
@@ -19,5 +19,28 @@ typedef struct{
 mtoka_osci_neuron_t *mtoka_osci_neuron_init(mtoka_osci_neuron_t *neuron);
 void mtoka_osci_neuron_destroy(mtoka_osci_neuron_t *neuron);
 
+#define mtoka_osci_neuron_rise_time_const(self)       (self)->tau
+#define mtoka_osci_neuron_adapt_time_const(self)      (self)->T
+#define mtoka_osci_neuron_mutual_inhibit_weight(self) (self)->a
+#define mtoka_osci_neuron_steady_firing_rate(self)    (self)->b
+#define mtoka_osci_neuron_tonic_input(self)           (self)->c
+#define mtoka_osci_neuron_sensory_feedback(self)      (self)->s
+#define mtoka_osci_neuron_membrane_potential(self)    (self)->x
+#define mtoka_osci_neuron_firing_rate(self)           (self)->y
+#define mtoka_osci_neuron_adapt_property(self)        (self)->v
+
+#define mtoka_osci_neuron_set_rise_time_const(self,tau)     \
+  ( mtoka_osci_neuron_rise_time_const(self) = (tau) )
+#define mtoka_osci_neuron_set_adapt_time_const(self,T)      \
+  ( mtoka_osci_neuron_adapt_time_const(self) = (T) )
+#define mtoka_osci_neuron_set_mutual_inhibit_weight(self,a) \
+  ( mtoka_osci_neuron_mutual_inhibit_weight(self) = (a) )
+#define mtoka_osci_neuron_set_steady_firing_rate(self,b)    \
+  ( mtoka_osci_neuron_steady_firing_rate(self) = (b) )
+#define mtoka_osci_neuron_set_tonic_input(self,c)           \
+  ( mtoka_osci_neuron_tonic_input(self) = (c) )
+#define mtoka_osci_neuron_set_sensory_feedback(self,s)      \
+  ( mtoka_osci_neuron_sensory_feedback(self) = (s) )
+
 
 #endif /* __RHC_MATSUOKA_OSCILLATOR_H__ */
--- test/rhc_matsuoka_oscillator_test.c
+++ test/rhc_matsuoka_oscillator_test.c
@@ -16,21 +16,87 @@ void teardown()
 TEST(test_mtoka_osci_neuron_init)
 {
   mtoka_osci_neuron_init( &neuron );
-  ASSERT_EQ( 1.0, neuron.tau );
-  ASSERT_EQ( 1.0, neuron.T );
-  ASSERT_EQ( 0.0, neuron.a );
-  ASSERT_EQ( 0.0, neuron.b );
-  ASSERT_EQ( 0.0, neuron.c );
-  ASSERT_EQ( 0.0, neuron.s );
-  ASSERT_EQ( 0.0, neuron.x );
-  ASSERT_EQ( 0.0, neuron.y );
-  ASSERT_EQ( 0.0, neuron.v );
+  ASSERT_EQ( 1.0, mtoka_osci_neuron_rise_time_const(&neuron) );
+  ASSERT_EQ( 1.0, mtoka_osci_neuron_adapt_time_const(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_mutual_inhibit_weight(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_steady_firing_rate(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_tonic_input(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_sensory_feedback(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_membrane_potential(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_firing_rate(&neuron) );
+  ASSERT_EQ( 0.0, mtoka_osci_neuron_adapt_property(&neuron) );
+}
+
+TEST(test_mtoka_osci_neuron_set_rise_time_const)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_rise_time_const( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_rise_time_const(&neuron) );
+  }
+}
+
+TEST(test_mtoka_osci_neuron_set_adapt_time_const)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_adapt_time_const( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_adapt_time_const(&neuron) );
+  }
+}
+
+TEST(test_mtoka_osci_neuron_set_mutual_inhibit_weight)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_mutual_inhibit_weight( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_mutual_inhibit_weight(&neuron) );
+  }
+}
+
+TEST(test_mtoka_osci_neuron_set_steady_firing_rate)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_steady_firing_rate( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_steady_firing_rate(&neuron) );
+  }
+}
+
+TEST(test_mtoka_osci_neuron_set_tonic_input)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_tonic_input( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_tonic_input(&neuron) );
+  }
+}
+
+TEST(test_mtoka_osci_neuron_set_sensory_feedback)
+{
+  double cases[] = {0.1, 0.3, 0.5, -1};
+  double *c;
+  for( c=cases; *c>0; c++ ){
+    mtoka_osci_neuron_set_sensory_feedback( &neuron, *c );
+    ASSERT_EQ( *c, mtoka_osci_neuron_sensory_feedback(&neuron) );
+  }
 }
 
 TEST(test_mtoka_osci_neuron)
 {
   CONFIGURE_SUITE(setup, teardown);
   RUN_TEST(test_mtoka_osci_neuron_init);
+  RUN_TEST(test_mtoka_osci_neuron_set_rise_time_const);
+  RUN_TEST(test_mtoka_osci_neuron_set_adapt_time_const);
+  RUN_TEST(test_mtoka_osci_neuron_set_mutual_inhibit_weight);
+  RUN_TEST(test_mtoka_osci_neuron_set_steady_firing_rate);
+  RUN_TEST(test_mtoka_osci_neuron_set_tonic_input);
+  RUN_TEST(test_mtoka_osci_neuron_set_sensory_feedback);
 }
 
 int main(int argc, char *argv[])
