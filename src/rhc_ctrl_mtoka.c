9f66d97865b6b9604be81c431e5ad175f87d099b
Hiroshi Atsuta <atsuta@ieee.org>
2022-09-30 17:52:01 +0900

    ctrl_mtoka: Fix calculation of reaction force

--- src/rhc_ctrl_mtoka.c
+++ src/rhc_ctrl_mtoka.c
@@ -54,6 +54,7 @@ ctrl_t *ctrl_mtoka_update(ctrl_t *self, double t, vec_t p)
   s = ctrl_mtoka_calc_sensory_feedback( self, ctrl_phi(self) );
   ctrl_mtoka_set_sensory_feedback( self, s );
   self->fz = ctrl_mtoka_calc_fz( self, t, p );
+  if( ctrl_phase_in( self, flight ) ) self->fz = 0;
   return self;
 }
 
