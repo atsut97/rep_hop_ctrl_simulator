b3598cc5e32f8577ac1a84f0d8a403f1f10f42e0
Hiroshi Atsuta <atsuta@ieee.org>
2025-01-10 11:29:35 +0900

    ctrl_rep_hop_stand: Add example for controller

---
+++ example/rep_hop_stand_test.c
@@ -0,0 +1,129 @@
+#include "rhc_ctrl_rep_hop_stand.h"
+#include "rhc_phase_portrait_plotter.h"
+#include "rhc_string.h"
+
+#define DT 0.001
+#define T  10
+
+cmd_t cmd;
+model_t model;
+ctrl_t ctrl;
+logger_t logger;
+ppp_t plotter;
+
+enum mode_list {
+  NONE = -1,
+  STAND = 0,
+  HOP,
+  SQUAT,
+};
+enum mode_list mode_id = NONE;
+
+
+
+void usage(int argc, const char *argv[])
+{
+  fprintf( stderr, "Usage:\n" );
+  fprintf( stderr, "  %s <mode>\n", argv[0] );
+  fprintf( stderr, "\n" );
+  fprintf( stderr, "  Acceptable mode names:\n" );
+  fprintf( stderr, "    stand\n" );
+  fprintf( stderr, "    hop\n" );
+  fprintf( stderr, "    squat\n" );
+}
+
+void parse(int argc, const char *argv[])
+{
+  if( argc < 2 ) {
+    mode_id = STAND;
+    return;
+  }
+
+  if( string_ends_with( argv[1], "help" ) ) {
+    usage( argc, argv );
+    exit( 0 );
+  } else if( strcmp( argv[1], "stand" ) == 0 )
+    mode_id = STAND;
+  else if( strcmp( argv[1], "hop" ) == 0 )
+    mode_id = HOP;
+  else if( strcmp( argv[1], "squat" ) == 0)
+    mode_id = SQUAT;
+  else {
+    fprintf( stderr, "unrecognized mode name: %s\n", argv[1] );
+    mode_id = NONE;
+    usage( argc, argv );
+    exit( 1 );
+  }
+}
+
+void init()
+{
+  cmd_default_init( &cmd );
+  model_init( &model, 10 );
+  logger_init( &logger );
+  ppp_init( &plotter, &cmd, &ctrl, &model, &logger );
+  ctrl_rep_hop_stand_create( &ctrl, &cmd, &model, no_soft_landing);
+}
+
+void make_edge_points(ppp_t *ppp)
+{
+  ppp_set_lim_xy( &plotter, 0.23, 0.29, -0.8, 0.8 );
+  ppp_set_n_sc_xy( ppp, 10, 10 );
+  ppp_generate_edge_points( ppp );
+}
+
+void set_params_stand()
+{
+}
+
+void set_params_hop()
+{
+}
+
+void set_params_squat()
+{
+}
+
+void setup_ctrl()
+{
+  make_edge_points( &plotter );
+  switch( mode_id ){
+  case STAND:
+    set_params_stand();
+    break;
+  case HOP:
+    set_params_hop();
+    break;
+  case SQUAT:
+    set_params_squat();
+    break;
+  case NONE:
+  default:
+    exit( 1 );
+    break;
+  }
+}
+
+void run()
+{
+  ppp_run( &plotter, T, DT );
+}
+
+void destroy()
+{
+  ppp_destroy( &plotter );
+  logger_destroy( &logger );
+  model_destroy( &model );
+  ctrl_destroy( &ctrl );
+  cmd_destroy( &cmd );
+}
+
+int main(int argc, const char *argv[])
+{
+  parse( argc, argv );
+  init();
+  setup_ctrl();
+  run();
+  destroy();
+  return 0;
+}
